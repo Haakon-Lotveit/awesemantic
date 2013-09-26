e7c9fd60f04792ff1d033480386f929bbd2e0799
kennilsen <kennilsen@gmail.com>
2013-09-26 15:19:27 +0200

    Persitance! Writes airports+iata to tdb.

--- src/no/uib/semanticweb/semanticflight/RDFLoader.java
+++ src/no/uib/semanticweb/semanticflight/RDFLoader.java
@@ -18,26 +18,23 @@
 
 package no.uib.semanticweb.semanticflight ;
 
-import java.io.InputStream;
-import java.util.Iterator;
-
 import no.uib.semanticweb.semanticflight.rdfstore.StoreRDF;
 
-import com.hp.hpl.jena.datatypes.RDFDatatype;
-import com.hp.hpl.jena.datatypes.TypeMapper;
 import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.DatasetFactory;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.tdb.TDBFactory;
-import com.hp.hpl.jena.util.FileManager;
-import com.hp.hpl.jena.vocabulary.VCARD;
+import com.hp.hpl.jena.update.GraphStore;
+import com.hp.hpl.jena.update.GraphStoreFactory;
+import com.hp.hpl.jena.update.UpdateExecutionFactory;
+import com.hp.hpl.jena.update.UpdateFactory;
+import com.hp.hpl.jena.update.UpdateProcessor;
+import com.hp.hpl.jena.update.UpdateRequest;
 
 /** Tutorial 2 resources as property values
  */
@@ -49,44 +46,59 @@ public class RDFLoader extends Object {
 
 	public void loadAirportsDbpedia () {
 
-
 		//		String queryString =
 		//				"PREFIX dbo: <http://dbpedia.org/ontology/>" +
 		//
-		//						"SELECT DISTINCT ?airports ?iata WHERE {" +
-		//						
-		//						"?airports dbo:iataLocationIdentifier ?iata ."+
-		//						
+		//						"CONSTRUCT {?airports dbo:iataLocationIdentifier ?iata} WHERE {" +
+		//
+		//						"?airports dbo:iataLocationIdentifier ?iata ." +												
 		//
 		//						"}";
-
-		String queryString =
+		String sparqlUpdateString = "" +
 				"PREFIX dbo: <http://dbpedia.org/ontology/>" +
 
-						"CONSTRUCT {?airports dbo:iataLocationIdentifier ?iata} WHERE {" +
+						"INSERT {?airports dbo:iataLocationIdentifier ?iata} WHERE {" +
+
+						"SERVICE <http://dbpedia.org/sparql> {" +
 
 						"?airports dbo:iataLocationIdentifier ?iata ." +												
 
-						"}";
+						"}}";
+
 
 		// Haakon: StoreRDF er en enkel wrapper. Akkurat nå gjør den ikke så mye annet enn å la deg skrive litegranne mindre.
 		StoreRDF store = StoreRDF.create();
-//		Dataset dataset = TDBFactory.createDataset("tdb/flightSTORE") ;
-//		Model model = dataset.getDefaultModel();
+
 		Dataset dataset = store.getDataset();
 		Model model = store.getModel();
 		System.out.println(model.size());
-
-		Query query = QueryFactory.create(queryString);
+		dataset.begin(ReadWrite.WRITE) ;
+//		Query query = QueryFactory.create(queryString);
 
 		// Execute the query and obtain results
-		QueryExecution qe = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
-		model = qe.execConstruct();
+//		QueryExecution qe = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+//		model = qe.execConstruct();
+		
+		try {
+		 GraphStore graphStore = GraphStoreFactory.create(dataset) ;
+		 
+		UpdateRequest request = UpdateFactory.create(sparqlUpdateString) ;
+	     UpdateProcessor proc = UpdateExecutionFactory.create(request, graphStore) ;
+	     proc.execute() ;
+		
 
 
 		//		model.write(System.out, "TURTLE");
 		System.out.println(model.size());
-		dataset = DatasetFactory.create(dataset);
+		System.out.println(dataset.getDefaultModel().size());
+//		dataset = DatasetFactory.create(model);
+		dataset.commit();
+//		dataset.end() ; 
+		
+//			dataset.commit() ;
+		} finally { 
+			dataset.end() ; 
+		}
 		//		model.write(System.out, "TURTLE");
 		//		ResultSetFormatter.out(System.out, results, query);
 
@@ -94,18 +106,35 @@ public class RDFLoader extends Object {
 				"PREFIX dbo: <http://dbpedia.org/ontology/>" +
 				"SELECT ?airports ?iata WHERE {" +
 				"?airports dbo:iataLocationIdentifier ?iata ." +
-				"}");
+				"}LIMIT 10");
 
-		QueryExecution queryExecution = QueryExecutionFactory.create(query2, dataset);
-        
-		ResultSet res = queryExecution.execSelect();
+//		QueryExecution queryExecution = QueryExecutionFactory.create(query2, dataset);
+//
+//		ResultSet res = queryExecution.execSelect();
+//
+//		while (res.hasNext()){
+//			System.out.println(res.next().toString());
+//		}
 
-		while (res.hasNext()){
-			System.out.println(res.next().toString());
-		}
-
-		dataset.close();
+//		dataset.begin(ReadWrite.WRITE) ;
 
 
+//		model.close();
+//		dataset.close();
+//
+		StoreRDF s = StoreRDF.create();
+		Dataset set = s.getDataset();
+		set.begin(ReadWrite.READ);
+		System.out.println(set.getDefaultModel().size());
+		set.end();
+//
+//		queryExecution = QueryExecutionFactory.create(query2, set);
+//
+//		res = queryExecution.execSelect();
+//
+//		while (res.hasNext()){
+//			System.out.println(res.next().toString());
+//		}
+//		dataset.close();
 	}
 }
