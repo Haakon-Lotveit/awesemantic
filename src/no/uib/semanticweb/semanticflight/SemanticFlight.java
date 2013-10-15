71a6f2b1f0db5053e675f4f1a3c38b77bf26b1ed
Erik <erikrols@gmail.com>
2013-10-15 12:47:41 +0200

    Fikset xmlting

--- src/no/uib/semanticweb/semanticflight/SemanticFlight.java
+++ src/no/uib/semanticweb/semanticflight/SemanticFlight.java
@@ -1,13 +1,20 @@
 package no.uib.semanticweb.semanticflight;
 
+import java.util.ArrayList;
+
 public class SemanticFlight {
 	
 	public static void main(String[] args){
 		//create an instance
 		SemanticXMLParser dpe = new SemanticXMLParser();
-
+		XmlSingle single = new XmlSingle();
+		ArrayList<ArrayList<String>> li = new ArrayList<ArrayList<String>>();
+		ArrayList<String> fly = new ArrayList<String>();
+		fly.add("BGO");
+		li.add(fly);
 		//call run example
 		dpe.runExample("OSL.xml");
+		single.connections(li, 0, 5);
 		
 		RDFLoader rdfLoader = new RDFLoader();
 		rdfLoader.loadAirportsDbpedia();
---
+++ src/no/uib/semanticweb/semanticflight/XmlSingle.java
@@ -0,0 +1,108 @@
+package no.uib.semanticweb.semanticflight;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class XmlSingle {
+
+
+	/**
+	 * @param args
+	 */
+	public void connections(ArrayList<ArrayList<String>> iata, int round, int depth){
+		if(round == depth){
+
+		}else{
+			for(int i = 0; i< iata.get(round).size(); i++){
+				long startTime = System.nanoTime();
+				String airport = iata.get(round).get(i); 
+				File f = new File("xml/xmlA/" + iata.get(round).get(i)+ "A.xml");
+				File f2 = new File("xml/xmlD/" + iata.get(round).get(i)+ "D.xml");
+				if(f.isFile()|f2.isFile()){
+
+				}
+				else{
+					URL url;
+					try {
+						url = new URL("http://flydata.avinor.no/XmlFeed.asp?airport="+ airport + "&direction=A");
+						Scanner s = new Scanner(url.openStream());
+						BufferedWriter writer = new BufferedWriter(new FileWriter("xml/xmlA/" + airport+ "A.xml"));
+						while(s.hasNext()){
+							writer.append(s.nextLine());
+							writer.newLine();
+						}
+						s.close();
+						writer.close();
+
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						url = new URL("http://flydata.avinor.no/XmlFeed.asp?airport="+ airport + "&direction=D");
+						Scanner s = new Scanner(url.openStream());
+						BufferedWriter writer = new BufferedWriter(new FileWriter("xml/xmlD/" + airport+ "D.xml"));
+						while(s.hasNext()){
+							writer.append(s.nextLine());
+							writer.newLine();
+						}
+						s.close();
+						writer.close();
+
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					BufferedReader reader;
+					try {
+						reader = new BufferedReader(new FileReader("xml/xmlD/" + airport+ "D.xml"));
+						String xml = "";
+						while(reader.readLine() != null){
+							xml = xml + reader.readLine();
+						}
+						reader.close();
+						reader = new BufferedReader(new FileReader("xml/xmlA/" + airport+ "A.xml"));
+						while(reader.readLine() != null){
+							xml = xml + reader.readLine();
+						}
+						reader.close();
+						String[]parts = xml.split("<airport>|<via_airport>|,");
+						String[]iAta = new String[parts.length-1];
+						ArrayList<String> sublist = new ArrayList<String>();
+						for(int j = 1; j < parts.length; j++){
+							String sub = parts[j].substring(0,3);
+							sublist.add(sub);
+							iAta[j-1] = sub;
+						}
+						iata.add(sublist);
+						connections(iata, round+1, depth);
+						//					for(int j = 0; j <iAta.length; j++){
+						//						File f = new File("xml/xmlA/" + iAta[j]+ "A.xml");
+						//						File f2 = new File("xml/xmlD/" + iAta[j]+ "D.xml");
+						//						if(f.isFile()|f2.isFile()){
+						//						}else{
+						//							connections(iAta[j], depth-1);
+						//						}
+						//					}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					long endTime = System.nanoTime();
+
+					long duration = endTime - startTime;
+					System.out.println(duration);
+				}
+			}
+		}
+	}
+}
